ab0ff6e48917f2972c3446b60b0d9bf4e56160d2
Ericp95 <ericp6960@gmail.com>
2021-10-04 17:13:00 -0300

    Update homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -126,6 +126,14 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  mayor=0;
+  for(m=0; m<numeros.length; m++){
+    if(numeros[m]>mayor){
+       mayor=numeros[m];
+    }
+  }
+   
+  return mayor;
 }
 
 
@@ -133,13 +141,28 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var producto=1;
+  if(arguments.length<1){
+    return 0;
+  }
+  
+  for(m=0; m<arguments.length; m++){
+    producto=producto*arguments[m];
+  }
+  return producto;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let contador= 0;
+  for(m=0; m<arreglo.lenght; m++){
+    if(arreglo[m]>18){
+      contador ++;
+    }
+  }
+  return contador;
 }
 
 
@@ -148,14 +171,23 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
-} 
+   
+  if(numeroDeDia === 1 || numeroDeDia === 7 ){
+    return "Es fin de semana";
+  }
+  return "Es dia Laboral";
+}
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let num = n.toString();
+  if(num.charAt(0) === "9"){
+    return true;
+  }
+  return false;
   
 }
 
@@ -163,8 +195,14 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  
+  //Escribe tu código aquí 
+  for (var i =0 ; i < arreglo.length - 1; i++) {
+    if(arreglo[i] !== arreglo[i+1]){
+      return false
+    }
+  }
+  return true
+ 
 } 
 
 
@@ -173,6 +211,21 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevoArray = [];
+  for( m= 0; m<array.length; m++) {
+    if(array[m] === "Enero" || array[m] === "Marzo" || array[m] === "Noviembre") {
+      nuevoArray.push(array[m]);
+    }
+  }
+  if(nuevoArray.length < 3) {
+    return "No se encontraron los meses pedidos";
+  }
+  
+  else {
+      return nuevoArray;
+  }
+
+  
 }
 
 
